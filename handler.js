0a903eedb19f8dc16b27bc879deec752b853d496
Daniel Zinkevich <dan@zivtech.com>
2015-11-05 15:02:46 -0800

    Renaming everything to Bitbucket.

--- handler.js
+++ handler.js
@@ -1,4 +1,4 @@
-var StashHandler = require('./lib/StashHandler');
+var BitbucketHandler = require('./lib/BitbucketHandler');
 
 var exports = function() {
   this.configure = this.configure.bind(this);
@@ -10,29 +10,29 @@ var exports = function() {
 var config = {};
 var server = {};
 
-exports.shortDescription = 'Runs a webhook handler and sends updates to stash status API.';
+exports.shortDescription = 'Runs a webhook handler and sends updates to bitbucket status API.';
 
 exports.help = 'Usage: npm start [args]';
 exports.help += '\n';
-exports.help += 'Provides a stash webhook endpoint.';
+exports.help += 'Provides a bitbucket webhook endpoint.';
 
 exports.options = function(yargs) {
   this.yargs = yargs;
   return yargs
     .describe('port', 'The port on which to listen for incoming requests.')
     .alias('port', 'p')
-    .describe('stash-webhook-path', 'The path at which to listen for webhooks.')
-    .alias('stash-webhook-path', 'P')
-    .describe('stash-webhook-secret', 'The webhook secret provided to Stash.')
-    .alias('stash-webhook-secret', 's')
-    .describe('stash-api-token', 'The API token to use to write to Stash.')
-    .alias('stash-api-token', 'a')
+    .describe('bitbucket-webhook-path', 'The path at which to listen for webhooks.')
+    .alias('bitbucket-webhook-path', 'P')
+    .describe('bitbucket-webhook-secret', 'The webhook secret provided to Bitbucket.')
+    .alias('bitbucket-webhook-secret', 's')
+    .describe('bitbucket-api-token', 'The API token to use to write to Bitbucket.')
+    .alias('bitbucket-api-token', 'a')
   ;
 };
 
 exports.configure = function(config) {
   config = config;
-  server = new StashHandler(config);
+  server = new BitbucketHandler(config);
 };
 
 exports.run = function(cb) {
